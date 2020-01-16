de55f18493b927e8b3011a466b5a96a480af5a6f
ShivamS1234 <shivammcu@gmail.com>
2020-01-16 18:20:09 +0530

    first commit VMS

---
+++ DemoVSM/DemoVSM/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoVSM
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+		string _currentColorState = "Normal";
+		public MainPage()
+        {
+            InitializeComponent();
+        }
+		void ToggleValid_OnClicked(object sender, EventArgs e)
+		{
+			if (_currentColorState == "Normal")
+			{
+				_currentColorState = "Invalid";
+			}
+			else
+			{
+				_currentColorState = "Normal";
+			}
+
+			VisualStateManager.GoToState(btnOk, _currentColorState);
+			VisualStateManager.GoToState(boxview1, _currentColorState);
+			VisualStateManager.GoToState(boxview2, _currentColorState);
+			VisualStateManager.GoToState(boxview3, _currentColorState);
+		}
+	}
+}
